ac5d69b309ce16f8e3137169da3096887c12c400
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-26 16:51:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write("Hello, *World!* :sunglasses:")
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.area_chart(chart_data)
