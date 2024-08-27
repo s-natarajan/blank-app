e11ab4a3b6a4a6299d857dfee161e3b5c2a73b8f
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-26 20:07:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from st_files_connection import FilesConnection
 
 st.title("🎈 My new app")
 st.write(
@@ -12,3 +13,8 @@ st.write("Hello, *World!* :sunglasses:")
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.area_chart(chart_data)
+conn = st.connection('s3', type=FilesConnection)
+df = conn.read("h4-hack-week-aug-2024/myfile.csv", input_format="csv", ttl=600)
+
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
